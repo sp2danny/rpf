d02c2d4b86286f03c623fd8cf35ff5fc2c2ec794
tooextdn <daniel.nystrom@external.atlascopco.com>
2018-09-26 13:15:18 +0200

    updated version number

--- src/main.cpp
+++ src/main.cpp
@@ -285,7 +285,7 @@ int main(int argc, char** argv)
 	}
 	else if ((argc==2) && (argv[1]=="--version"s))
 	{
-		std::cout << "Reverse Polish Find v1.02" << std::endl;
+		std::cout << "Reverse Polish Find v1.03" << std::endl;
 	}
 	else try
 	{
